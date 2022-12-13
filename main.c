24f816bb52eb57ccdee3b5fa6f2b01b8a5854549
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-12-12 23:22:59 -0300

    Create APP with bubbleSort

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int v[], int i, int j) {
+    int aux = v[i];
+    v[i] = v[j];
+    v[j] = aux;
+}
+
+int bubbleSort(int v[], int n) {
+    int contador = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            contador++;
+            if (v[i] > v[j]) {
+                swap(v, i, j);
+            }
+        }
+    }
+
+    return contador;
+}

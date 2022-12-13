ea2304558613824028bef99721043fd1b0987350
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-12-12 23:31:00 -0300

    add: cria funcoes de mock de caso

--- main.c
+++ main.c
@@ -3,4 +3,33 @@
 
 void main() {
 
+    int* melhorCaso(int n) {
+        int* vetor = malloc(sizeof(int) * n);
+
+        for (int i = 0; i < n; i++) {
+            vetor[i] = i;
+        }
+
+        return vetor;
+    }
+
+    int* piorCaso(int n) {
+        int* vetor = malloc(sizeof(int) * n);
+
+        for (int i = 0; i < n; i++) {
+            vetor[i] = (n - 1) - i;
+        }
+
+        return vetor;
+    }
+
+    int* casoMedio(int n) {
+        int* vetor = malloc(sizeof(int) * n);
+
+        for (int i = 0; i < n; i++) {
+            vetor[i] = rand() % n;
+        }
+
+        return vetor;
+    }
 }
